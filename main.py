d9b4fd3ab6e06cefbe7d099d5ccdd2c7ca6a427c
Akshat Jain <jainakshatkumar@gmail.com>
2024-07-13 17:47:37 +0530

    Flipkart webscrapping script created

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from src.amazon_webscrapping import Amazon
+from src.flipkart_webscrapping import Flipkart
 import pandas as pd
 
 def main():
-    url = 'https://www.amazon.in/HP-i5-1235U-15-6-inch-graphics-speakers/dp/B0CTKHTNWL/ref=sr_1_4?dib=eyJ2IjoiMSJ9.9zhNTtyM6wB18-nU56oobiIA_iHvbszw-zCKLZGEvW5qrNqEtAX6yfYSwYe4H8pURPZVTeXA6cK8xFa_yNDypRetrHBhVEkrSjwfRQe3X7aZsMfe43doKFkIlLO0vvq6ysy9qd398TlGCfEPX8nSqCf7gIJwIqKoANeOSpzL8dNxRvN4wE79-ZoDQ5AIKuiudxTkGJ5p46io1n4x-QUfI9PdYG7qPhQ9o9Rkb-nOW2k.UcuOKL58ddW5gGa4mLWzXUDeUaTIVr9rLtQ3ZgQwEd0&dib_tag=se&keywords=laptop&qid=1720863278&sr=8-4&th=1'
-    amazon_scraper = Amazon(url)
-    dict = amazon_scraper.get_all_reviews()
+    url = 'https://www.flipkart.com/apple-iphone-15-pink-128-gb/p/itm7579ed94ca647?pid=MOBGTAGPNMZA5PU5&lid=LSTMOBGTAGPNMZA5PU51YWTGZ&marketplace=FLIPKART&q=mobile+iphone&store=tyy%2F4io&srno=s_1_4&otracker=search&otracker1=search&fm=Search&iid=e73c2247-b211-47e1-b39f-ec078d92f2b9.MOBGTAGPNMZA5PU5.SEARCH&ppt=pp&ppn=pp&ssid=p2illcz7800000001720872195500&qH=0747fed614fc520e'
+    # amazon_scraper = Amazon(url)
+    # dict = amazon_scraper.get_all_reviews()
+
+    flipkart_scrapper = Flipkart(url)
+    dict = flipkart_scrapper.get_all_reviews()
 
     df = pd.DataFrame(dict, columns = dict.keys())
     print(df)
