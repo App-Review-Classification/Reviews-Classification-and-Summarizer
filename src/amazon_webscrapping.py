929b736e75ce8d8eb6b4636ef828d68d1890f5d7
Akshat Jain <jainakshatkumar@gmail.com>
2024-07-13 14:49:19 +0530

    Amazon websrapping done

--- src/amazon_webscrapping.py
+++ src/amazon_webscrapping.py
@@ -12,22 +12,27 @@ class Amazon:
         self.url = url
         self.driver = None
         self.review_page_url = None
+        self.main_dict = {}
+        self.main_dict["Star"] = []
+        self.main_dict["Summary"] = []
+        self.main_dict["Description"] = []
+        self.page_count = 0
     
     def open_link(self):
         options = Options()
         options.page_load_strategy = 'normal'
         self.driver = webdriver.Chrome(options=options)
         self.driver.get(self.url)
+        # self.driver.implicitly_wait(60)
     
     def get_reviewpage(self):
         try: 
             self.open_link()
 
             review_footer_xpath = "//*[@id='reviews-medley-footer']"
-            WebDriverWait(self.driver, 20).until(
-                EC.presence_of_element_located((By.XPATH, review_footer_xpath))
-            )
-            
+            # WebDriverWait(self.driver, 20).until(
+            #     EC.presence_of_element_located((By.XPATH, review_footer_xpath))
+            # )
             review_footer = self.driver.find_element(By.XPATH, review_footer_xpath)
             review_link = None
             if review_footer:
@@ -36,37 +41,109 @@ class Amazon:
                 review_link = review_tag.get_attribute("href")
 
                 if review_link:
-                    print(f"Review Link found : {review_link}")
+                    print(f"INFO | Review Link found.")
+                    print(f"INFO | {review_link}")
                     self.review_page_url = review_link
-                    print(f"Opening Review page.\n")
+                    print(f"INFO | Opening Review page.")
                     self.driver.get(self.review_page_url)
+                    return
                 else:
-                    print("Review Link not found")
+                    print("ERROR | Review Link not found")
             else:
-                print("Review footer not found")
+                print("ERROR | Review footer not found")
         except Exception as e:
-            print(f"Review footer not found \n Error : {e}")
+            print(f"ERROR | Review footer not found Error : {e}")
+
+    def extract_reviews(self):
+        try:
+            # self.get_reviewpage()
+            review_list_xpath = "//*[@id='cm_cr-review_list']"
+            review_list = self.driver.find_element(By.XPATH, review_list_xpath)
+            reviews = review_list.find_elements(By.CSS_SELECTOR, "div[data-hook='review']")
+
+            if review_list:
+
+                print(f"INFO | Total reviews to fetch from page {self.page_count} are {len(reviews)}")
+                print(f"INFO | Fetching reviews from page {self.page_count}")
+                for review in reviews:
+                    review_id = review.get_attribute("id")
+                    review_star_xpath = "//*[@id='customer_review-" + str(review_id) + "']/div[2]/a/i/span"
+                    review_summary_xpath = "//*[@id='customer_review-" + str(review_id) + "']/div[2]/a/span[2]"
+                    review_description_xpath = "//*[@id='customer_review-" + str(review_id) + "']/div[4]/span/span"
+
+                    try: 
+                        review_star = review.find_element(By.XPATH, review_star_xpath).text
+                    except Exception as e:
+                        review_star = None
+                    
+                    try:
+                        review_summary = review.find_element(By.XPATH, review_summary_xpath).text
+                    except Exception as e:
+                        review_summary = None
+                    
+                    try: 
+                        review_description = review.find_element(By.XPATH, review_description_xpath).text
+                    except Exception as e:
+                        review_description = None
+
+                    if review_star:
+                        self.main_dict['Star'].append(review_star)
+                    else:
+                        self.main_dict['Star'].append(None)
+
+                    if review_summary:
+                        self.main_dict['Summary'].append(review_summary)
+                    else:
+                        self.main_dict['Summary'].append(None)
+
+                    if review_description:
+                        self.main_dict['Description'].append(review_description)
+                    else:
+                        self.main_dict['Description'].append(None)
+
+                print(f"INFO | {len(reviews)} reviews from page {self.page_count} appended in dictionary.")
+            else:
+                print(f"INFO | Review list not found on page {self.page_count}.")
+
+        except Exception:
+            print(f"ERROR | Exception occured in extract_reviews function for page {self.page_count}.") 
+    
+    def get_all_reviews(self):
+        try:
+            self.get_reviewpage()
+
+            while(1):
+                self.page_count += 1
+                print(f"INFO | Etracting reviews from page {self.page_count}...")
+                self.extract_reviews()
+                
+                print(f"INFO | Searching for next page")
+                next_page_xpath = "//*[@id='cm_cr-pagination_bar']/ul/li[2]/a"
+                if not next_page_xpath:
+                    print(f"INFO | Page {self.page_count} not found")
+                    break
+                next_page_a = self.driver.find_element(By.XPATH, next_page_xpath)
+                if(next_page_a):
+                    next_page_url = next_page_a.get_attribute("href")
+                    if(next_page_url):
+                        print(f"INFO | Next page found")
+                        print(f"INFO | Next page loading...")
+                        self.driver.get(str(next_page_url))
+                        print(f"INFO | Next page loaded successfully.")
+                    else:
+                        print(f"INFO | URL not found for page {self.page_count}")
+                        break
+                else:
+                    print("INFO | No page left")
+                    break
+        
+        except Exception:
+            print("ERROR | Exception occured in get_all_reviews function")
+        
+        return self.main_dict
+        self.close()
     
     def close(self):
         if self.driver:
             self.driver.quit()
-
-    def extract_reviews(self):
-        try:
-            self.open_link()
-            # Wait until reviews are loaded
-            WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, '.review'))
-            )
-            
-            reviews = self.driver.find_elements(By.CSS_SELECTOR, '.review')
-            review_texts = [review.text for review in reviews]
-            return review_texts
-
-        except TimeoutException:
-            print("Timed out waiting for page to load")
-            return []
-
-        finally:
-            self.close()
 
